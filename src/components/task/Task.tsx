6254ccd7972d6618eea22c484aa509b7e5295335
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-27 21:55:02 +0300

    changed Task.tsx and style for this

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,20 +1,23 @@
 import {TasksType} from "../todolist/Todolist.tsx";
 import style from './task.module.scss'
 import {ChangeEvent} from "react";
+import {ChangeableSpan} from "../changeableSpan/ChangeableSpan.tsx";
 
 type TasksPropsType = TasksType & {
     todoId: string
     removeTask: (todoId: string, taskId: string) => void
     changeTaskStatus: (todoId: string, id: string, newValue: boolean) => void
+    changeText: (newTitle: string) => void
 }
-export const Task = ({id, todoId,isDone, title, removeTask, changeTaskStatus}: TasksPropsType) => {
+export const Task = ({id, todoId, isDone, title, removeTask, changeTaskStatus, changeText}: TasksPropsType) => {
     const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => changeTaskStatus(todoId, id, e.currentTarget.checked)
     const removeTaskHandler = () => removeTask(todoId, id)
     const isDoneStyle = isDone ? style.isDone : ''
     return (
         <li className={`${style.wrapper} ${isDoneStyle}`} key={id}>
-            <input type="checkbox" checked={isDone} onChange={changeTaskStatusHandler}/>
-            <span className={style.text}>{title}</span>
+            <input className={style.checkBox} type="checkbox" checked={isDone} onChange={changeTaskStatusHandler}/>
+            <ChangeableSpan text={title} changeText={changeText}/>
+            {/*<span className={style.text}>{title}</span>*/}
             <button className={style.button} onClick={removeTaskHandler}>x</button>
         </li>
     );
