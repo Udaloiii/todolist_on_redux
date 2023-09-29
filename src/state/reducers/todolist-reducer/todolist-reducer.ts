a63fe8446ea1ec1f0c23cc02c716fe4cbc4368a7
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-29 15:20:03 +0300

    changed project

---
+++ src/state/reducers/todolist-reducer/todolist-reducer.ts
@@ -0,0 +1,61 @@
+import {FiltersType, TodolistsType} from "src/App.tsx";
+import {v1} from "uuid";
+
+const initialState: TodolistsType[] = []
+
+// types for actions type
+type AddTodolistType = {
+    type: 'ADD-TODOLIST'
+    title: string
+}
+type RemoveTodolistType = {
+    type: 'REMOVE-TODOLIST'
+    todoId: string
+}
+type ChangeTodolistTitleType = {
+    type: 'CHANGE-TODOLIST-TITLE'
+    todoId: string
+    newTitle: string
+}
+type ChangeTodolistFilterType = {
+    type: 'CHANGE-TODOLIST-FILTER'
+    todoId: string
+    newValue: FiltersType
+}
+
+
+type ActionsType = AddTodolistType | RemoveTodolistType | ChangeTodolistTitleType | ChangeTodolistFilterType
+export const todolistReducer = (state = initialState, action: ActionsType) => {
+    switch (action.type) {
+
+        case "ADD-TODOLIST":
+            let newTodolist: TodolistsType = {id: v1(), title: action.title, filter: "all"}
+            return [newTodolist, ...state]
+
+        case "REMOVE-TODOLIST":
+            return state.filter(el => el.id !== action.todoId)
+
+        case "CHANGE-TODOLIST-TITLE":
+            return state.map(el => el.id === action.todoId ? {...el, title: action.newTitle} : el)
+
+        case "CHANGE-TODOLIST-FILTER":
+            return state.map(el => el.id === action.todoId ? {...el, filter: action.newValue} : el)
+
+        default:
+            return state
+    }
+}
+
+// Action Creators
+export const addTodolistAC = (title: string) => {
+    return {type: 'ADD-TODOLIST', title} as const
+}
+export const removeTodolistAC = (todoId: string) => {
+    return {type: 'REMOVE-TODOLIST', todoId} as const
+}
+export const changeTodolistTitleAC = (todoId: string, newTitle: string) => {
+    return {type: 'CHANGE-TODOLIST-TITLE', todoId, newTitle} as const
+}
+export const changeTodolistFilterAC = (todoId: string, newValue: FiltersType) => {
+    return {type: 'CHANGE-TODOLIST-FILTER', todoId, newValue} as const
+}
