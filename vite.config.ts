3224df39acdf157b71c2a611cb884f180a737102
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-01 15:31:30 +0300

    added alias and type for this in project

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3001,
-    open: true,
-  }, // поменял порт из-за cors-ошибки ( запросы на сервер social-network разрешено только с localhost:3000, localhost:3001, localhost:9009)
+    plugins: [react()],
+    resolve: {
+        alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
+    },
+    server: {
+        host: 'localhost',
+        port: 3001,
+        open: true,
+    }, // поменял порт из-за cors-ошибки ( запросы на сервер social-network разрешено только с localhost:3000, localhost:3001, localhost:9009)
 })
