cb456c4cd861dcfe1340c34927eecc744de03f70
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-01 20:19:47 +0300

    added function onEnterPress (improvement) in AddItemForm.tsx and refactor

--- src/components/addItemForm/AddItemForm.tsx
+++ src/components/addItemForm/AddItemForm.tsx
@@ -1,24 +1,33 @@
 import style from './AddItemForm.module.scss'
-import {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 
 type AddItemFormProps = {
     className?: string
     addItem: (text: string) => void
 }
-export const AddItemForm = ({className, addItem}: AddItemFormProps) => {
-    const [text, setText] = useState<string>("")
+export const AddItemForm = React.memo(({className, addItem}: AddItemFormProps) => {
+        const [text, setText] = useState<string>("")
 
-    const onChangeTextHandler = (e: ChangeEvent<HTMLInputElement>) => setText(e.currentTarget.value)
-    const addItemHandler = () => {
-        if(text.trim() !== "") {
-            addItem(text.trim())
-            setText('')
+        const onChangeTextHandler = (e: ChangeEvent<HTMLInputElement>) => setText(e.currentTarget.value)
+        const addItemHandler = () => {
+            if (text.trim() !== "") {
+                addItem(text.trim())
+                setText('')
+            }
         }
+        const onEnterPress = (event: KeyboardEvent<HTMLInputElement>) => {
+            if (event.key === 'Enter') {
+                addItemHandler()
+                event.currentTarget.blur()
+            }
+        }
+
+        return (
+            <div className={`${style.wrapper} ${className}`}>
+                <input type="text" placeholder={'enter text'} value={text} onChange={onChangeTextHandler}
+                       onKeyDown={onEnterPress}/>
+                <button onClick={addItemHandler}>+</button>
+            </div>
+        );
     }
-    return (
-        <div className={`${style.wrapper} ${className}`}>
-            <input type="text" placeholder={'enter text'} value={text} onChange={onChangeTextHandler}/>
-            <button onClick={addItemHandler}>+</button>
-        </div>
-    );
-};
+)
