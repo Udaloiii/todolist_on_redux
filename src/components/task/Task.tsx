c3a2add2cde0ad18715b5c68cbcf90f803ff56f1
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-03 21:55:19 +0300

    refactored Task.tsx and Todolist.tsx

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -2,12 +2,13 @@ import {TasksType} from "../todolist/Todolist.tsx";
 import style from './task.module.scss'
 import React, {ChangeEvent, useCallback} from "react";
 import {ChangeableSpan} from "../changeableSpan/ChangeableSpan.tsx";
+import {TaskStatuses} from "@/api/mainApi.ts";
 
 type TasksPropsType = TasksType & {
     id: string
     todoId: string
     removeTask: (todoId: string, taskId: string) => void
-    changeTaskStatus: (todoId: string, id: string, newValue: boolean) => void
+    changeTaskStatus: (todoId: string, id: string, status: TaskStatuses) => void
     changeText: (newTitle: string) => void
 }
 export const Task = React.memo(({
@@ -19,7 +20,10 @@ export const Task = React.memo(({
                                     changeTaskStatus,
                                     changeText
                                 }: TasksPropsType) => {
-    const changeTaskStatusHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => changeTaskStatus(todoId, id, e.currentTarget.checked), [todoId, id])
+    const changeTaskStatusHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        const checked = e.currentTarget.checked
+        changeTaskStatus(todoId, id, checked ? TaskStatuses.Completed : TaskStatuses.New)
+    }, [todoId, id])
     const removeTaskHandler = useCallback(() => removeTask(todoId, id), [todoId, id])
     const changeTextHandler = useCallback(() => changeText, [])
     const isDoneStyle = isDone ? style.isDone : ''
--- src/components/todolist/Todolist.tsx
+++ src/components/todolist/Todolist.tsx
@@ -3,7 +3,10 @@ import style from './todolist.module.scss'
 import {AddItemForm} from "../addItemForm/AddItemForm.tsx";
 import {FiltersType} from "../../App.tsx";
 import {ChangeableSpan} from "../changeableSpan/ChangeableSpan.tsx";
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
+import {getTasksTC} from "@/state/reducers/task-reducer.ts";
+import {useAppCustomDispatch} from "@/state/state.ts";
+import {TaskFromBack, TaskStatuses} from "@/api/mainApi.ts";
 
 export type TasksType = {
     id: string
@@ -13,10 +16,10 @@ export type TasksType = {
 type TodolistPropsType = {
     id: string
     title: string
-    tasks: TasksType[]
+    tasks: TaskFromBack[]
     addTask: (todolistId: string, text: string) => void
     removeTask: (todolistId: string, taskId: string) => void
-    changeTaskStatus: (todolistId: string, id: string, newValue: boolean) => void
+    changeTaskStatus: (todolistId: string, id: string, status: TaskStatuses) => void
     changeFilter: (todoId: string, value: FiltersType) => void
     filter: FiltersType
     removeTodolist: (todoId: string) => void
@@ -37,6 +40,9 @@ export const Todolist = React.memo(({
                                         changeText
                                     }: TodolistPropsType) => {
 
+    const dispatch = useAppCustomDispatch()
+
+    // functions
     const addTaskHandler = useCallback((text: string) => addTask(id, text), [addTask, id])
     const removeTodolistHandler = useCallback(() => removeTodolist(id), [removeTodolist, id])
     const changeTitleHandler = useCallback((newTitle: string) => changeTitle(id, newTitle), [changeTitle, id])
@@ -48,11 +54,15 @@ export const Todolist = React.memo(({
 
     let filteredTask = tasks
     if (filter === "active") {
-        filteredTask = tasks.filter(el => !el.isDone)
+        filteredTask = tasks.filter(el => el.status !== TaskStatuses.Completed)
     }
     if (filter === "completed") {
-        filteredTask = tasks.filter(el => el.isDone)
+        filteredTask = tasks.filter(el => el.status !== TaskStatuses.Completed)
     }
+
+    useEffect(() => {
+        dispatch(getTasksTC(id))
+    }, [])
     return (
         <div className={style.box}>
             <h3 className={style.changeableSpanBox}>
@@ -66,9 +76,11 @@ export const Todolist = React.memo(({
             <ul>
                 {filteredTask?.map(el => {
                     const changeTextHandler = (newTitle: string) => changeText(id, el.id, newTitle)
-                    return (<Task key={el.id} id={el.id} todoId={id} title={el.title} isDone={el.isDone}
-                                  removeTask={removeTask} changeTaskStatus={changeTaskStatus}
-                                  changeText={changeTextHandler}/>)
+                    return (
+                        <Task key={el.id} id={el.id} todoId={id} title={el.title} isDone={el.status === 2}
+                              removeTask={removeTask} changeTaskStatus={changeTaskStatus}
+                              changeText={changeTextHandler}/>
+                    )
                 })}
             </ul>
             <div className={style.buttonsBlock}>
