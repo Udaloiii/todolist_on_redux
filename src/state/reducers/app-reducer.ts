949f3940e5f925c1956e0dd276d1e6fa0921c45c
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-10 23:30:16 +0300

    added property isInitialized for project

--- src/state/reducers/app-reducer.ts
+++ src/state/reducers/app-reducer.ts
@@ -4,19 +4,23 @@ export type AppErrorType = string | null
 export type InitialStateType = {
     status: AppStatusType
     error: AppErrorType
+    isInitialized: boolean
 }
 const initialState: InitialStateType = {
     status: 'loading',
-    error: null
+    error: null,
+    isInitialized: false
 }
 
-type ActionType = ReturnType<typeof appStatusAC> | ReturnType<typeof appErrorAC>
+type ActionType = ReturnType<typeof appStatusAC> | ReturnType<typeof appErrorAC> | ReturnType<typeof appInitializedAC>
 export const appReducer = (state = initialState, action: ActionType) => {
     switch (action.type) {
         case 'SET-APP-STATUS':
             return {...state, status: action.status}
         case 'SET-APP-ERROR' :
             return {...state, error: action.error}
+        case 'SET-APP-INITIALIZED' :
+            return {...state, isInitialized: action.isInitialized}
         default:
             return state
     }
@@ -30,4 +34,8 @@ export const appStatusAC = (status: AppStatusType) => {
 
 export const appErrorAC = (error: AppErrorType) => {
     return {type: 'SET-APP-ERROR', error} as const
+}
+
+export const appInitializedAC = (isInitialized: boolean) => {
+    return {type: 'SET-APP-INITIALIZED', isInitialized} as const
 }
