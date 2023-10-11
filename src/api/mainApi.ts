773ce23b1a35db17cd987956df1f68b173a3b9fc
Udaloiii <udaloiiisohappy@gmail.com>
2023-10-11 17:44:43 +0300

    fix bug

--- src/api/mainApi.ts
+++ src/api/mainApi.ts
@@ -3,7 +3,10 @@ import {FormikValuesType} from "@/components/login/Login.tsx";
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'bfb2a9e7-51b4-4737-9da8-c82e19cdd77e'
+    }
 })
 
 // types from backend
